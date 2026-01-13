922d8091320e9e06e8e41886089a96d2c0adc76b
alia <alia@hildebrand.co.uk>
2026-01-13 10:28:13 +0000

    feat: add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app-config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Pulseboard API')
+    .setDescription('Pulseboard API docs')
+    .setVersion('0.1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   const config = app.get<AppConfigService>(AppConfigService);
   await app.listen(config.port);
 }
